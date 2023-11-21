4ab9fc2f3945633cb71ad3665a028d72f0c31f2a
Mohit Pala <palav@appstate.edu>
2023-11-21 14:51:08 -0500

    changer skeleton

--- src/app/theme-designer/changer/changer.component.ts
+++ src/app/theme-designer/changer/changer.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
 	styleUrls: ['./changer.component.css']
 })
 export class ChangerComponent {
-	
+	public toChange: String[] = [
+		'Background',
+		'Foreground',
+		'Button',
+		'Border',
+	];
+
+	public redSlider: number = 0;
+	public greenSlider: number = 0; 
+	public blueSlider: number = 0;
+
 }
