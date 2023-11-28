2b2b7f879e0e2ee13f62feb33cea8a77afe7508a
Mohit Pala <palav@appstate.edu>
2023-11-28 14:11:44 -0500

    added save array

--- src/app/theme-load/theme-load.component.ts
+++ src/app/theme-load/theme-load.component.ts
@@ -6,28 +6,183 @@ import { Component } from '@angular/core';
 	styleUrls: ['./theme-load.component.css']
 })
 export class ThemeLoadComponent {
-	public backR: number = 0;
-	public backG: number = 0;
-	public backB: number = 0;
+	public backR: number = 30;
+	public backG: number = 30;
+	public backB: number = 46;
 
-	public foreR: number = 0;
-	public foreG: number = 0;
-	public foreB: number = 0;
+	public foreR: number = 205;
+	public foreG: number = 214;
+	public foreB: number = 244;
 
-	public buttonR: number = 0;
-	public buttonG: number = 0;
-	public buttonB: number = 0;
+	public buttonR: number = 49;
+	public buttonG: number = 50;
+	public buttonB: number = 68;
 
-	public borderR: number = 0;
-	public borderG: number = 0;
-	public borderB: number = 0;
+	public borderR: number = 242;
+	public borderG: number = 205;
+	public borderB: number = 205;
 
 	public makeRGB(redColor: number, greenColor: number, blueColor: number): String {
-		return 'RGB(' + redColor.toString() + ', ' + greenColor.toString() + ', ' + blueColor.toString() + ')';
+		return 'rgb(' + redColor + ',' + greenColor + ',' + blueColor + ')';
 	}
 
 	public backColor: String = this.makeRGB(this.backR, this.backG, this.backB);
 	public foreColor: String = this.makeRGB(this.foreR, this.foreG, this.foreB);
 	public buttonColor: String = this.makeRGB(this.buttonR, this.buttonG, this.buttonB);
 	public borderColor: String = this.makeRGB(this.borderR, this.borderG, this.borderB);
+
+	public themeName: String = 'Red-Yellow theme';
+
+	public savingTheme: boolean = false;
+
+	public themesArray: {
+		name: String,
+		background: String,
+		foreground: String,
+		button: String,
+		border: String
+	}[] = [
+
+	]
+
+	public toChange: { name: String, id: number }[] = [
+		{ 'name': 'Background', 'id': 0 },
+		{ 'name': 'Foreground', 'id': 1 },
+		{ 'name': 'Button', 'id': 2 },
+		{ 'name': 'Border', 'id': 3 },
+	];
+
+	public redSlider: number = 0;
+	public greenSlider: number = 0;
+	public blueSlider: number = 0;
+	public currentlyEditing: number = 10;
+
+	parseId(id: number) {
+		if (id === 0) {
+			this.currentlyEditing = 0;
+			this.redSlider = this.backR;
+			this.greenSlider = this.backG;
+			this.blueSlider = this.backB;
+		}
+		else if (id === 1) {
+			this.currentlyEditing = 1;
+			this.redSlider = this.foreR;
+			this.greenSlider = this.foreG;
+			this.blueSlider = this.foreB;
+		}
+		else if (id === 2) {
+			this.currentlyEditing = 2;
+			this.redSlider = this.buttonR;
+			this.greenSlider = this.buttonG;
+			this.blueSlider = this.buttonB;
+		}
+		else if (id === 3) {
+			this.currentlyEditing = 3;
+			this.redSlider = this.borderR;
+			this.greenSlider = this.borderG;
+			this.blueSlider = this.borderB;
+		}
+		else {
+			this.currentlyEditing = 10;
+		}
+		console.log(this.currentlyEditing);
+	}
+
+	public updateRed(): void {
+		if (this.currentlyEditing === 0) {
+			this.backR = this.redSlider;
+			this.backColor = this.makeRGB (this.backR, this.backG, this.backB);
+		}
+		else if (this.currentlyEditing === 1) {
+			this.foreR = this.redSlider;
+			this.foreColor = this.makeRGB(this.foreR, this.foreG, this.foreB);
+		}
+		else if (this.currentlyEditing === 2) {
+			this.buttonR = this.redSlider;
+			this.buttonColor = this.makeRGB(this.buttonR, this.buttonG, this.buttonB);
+		}
+		else if (this.currentlyEditing === 3) {
+			this.borderR = this.redSlider;
+			this.borderColor = this.makeRGB(this.borderR, this.borderG, this.borderB);
+		}
+	}
+
+	public updateGreen(): void {
+		if (this.currentlyEditing === 0) {
+			this.backG = this.greenSlider;
+			this.backColor = this.makeRGB (this.backR, this.backG, this.backB);
+		}
+		else if (this.currentlyEditing === 1) {
+			this.foreG = this.greenSlider;
+			this.foreColor = this.makeRGB(this.foreR, this.foreG, this.foreB);
+		}
+		else if (this.currentlyEditing === 2) {
+			this.buttonG = this.greenSlider;
+			this.buttonColor = this.makeRGB(this.buttonR, this.buttonG, this.buttonB);
+		}
+		else if (this.currentlyEditing === 3) {
+			this.borderG = this.greenSlider;
+			this.borderColor = this.makeRGB(this.borderR, this.borderG, this.borderB);
+		}
+	}
+
+	public updateBlue(): void {
+		if (this.currentlyEditing === 0) {
+			this.backB = this.blueSlider;
+			this.backColor = this.makeRGB (this.backR, this.backG, this.backB);
+		}
+		else if (this.currentlyEditing === 1) {
+			this.foreB = this.blueSlider;
+			this.foreColor = this.makeRGB(this.foreR, this.foreG, this.foreB);
+		}
+		else if (this.currentlyEditing === 2) {
+			this.buttonB = this.blueSlider;
+			this.buttonColor = this.makeRGB(this.buttonR, this.buttonG, this.buttonB);
+		}
+		else if (this.currentlyEditing === 3) {
+			this.borderB = this.blueSlider;
+			this.borderColor = this.makeRGB(this.borderR, this.borderG, this.borderB);
+		}
+	}
+
+	public resetTo0(): void {
+		this.backR = 255;
+		this.backG = 255;
+		this.backB = 255;
+		this.foreR = 0;
+		this.foreG = 0;
+		this.foreB = 0;
+		this.buttonR = 255;
+		this.buttonG = 255;
+		this.buttonB = 255;
+		this.borderR = 255;
+		this.borderG = 255;
+		this.borderB = 255;
+		this.backColor = this.makeRGB (this.backR, this.backG, this.backB);
+		this.foreColor = this.makeRGB(this.foreR, this.foreG, this.foreB);
+		this.buttonColor = this.makeRGB(this.buttonR, this.buttonG, this.buttonB);
+		this.borderColor = this.makeRGB(this.borderR, this.borderG, this.borderB);
+	}
+
+	public showSave(): void {
+		this.savingTheme = true;
+	}
+
+	public addToArray(): void {
+		this.themesArray.push(
+			{
+				'name': this.themeName,
+				'background': this.backColor,
+				'foreground': this.foreColor,
+				'button': this.buttonColor,
+				'border': this.borderColor
+			}
+		);
+	}
+
+	public saveTheme(): void {
+		this.addToArray();
+		this.savingTheme = false;
+		this.themeName = 'Red-Yellow Theme';
+	}
 }
